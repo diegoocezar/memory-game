085ce0f560282bec09a620f364fd3d77c9be094f
diegoocezar <diegocezar.comercial@gmail.com>
2019-04-23 19:23:11 +0000

    Refatoração das functions e início da aula 025

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,27 +1,21 @@
-function createMemoryCard() {
-  const $memoryCard = `<article class="memory-card">
-                          <img 
-                              src='img/icon-collabcode.png' 
-                              alt='Gueio mascote da Collabcode' 
-                              class='icon' onClick="handleClick()">
-                          </img> 
-                        <article>`;
+const createMemoryCard = () => `
+  <article class="memory-card">
+    <img 
+        src='img/icon-collabcode.png' 
+        alt='Gueio mascote da Collabcode' 
+        class='icon' onClick="handleClick()">
+    </img> 
+  <article>
+`;
 
-  return $memoryCard;
-}
+const creatememoryCardTurned = () => `
+  <article class="memory-card -turned">
+    <img 
+        src='img/icon-c.png' 
+        alt='Ícone de um livro da linguagem c++' 
+        class='icon' onClick="handleClick()">
+    </img> 
+  <article>
+`;
 
-function creatememoryCardTurned() {
-  const $memoryCardTurned = `<article class="memory-card -turned">
-                                <img 
-                                    src='img/icon-c.png' 
-                                    alt='Ícone de um livro da linguagem c++' 
-                                    class='icon' onClick="handleClick()">
-                                </img> 
-                              <article>`;
-
-  return $memoryCardTurned;
-}
-
-function handleClick(){
-  console.log("Ae");
-}
+const handleClick = () => console.log("Ae");
