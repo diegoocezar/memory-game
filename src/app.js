bf79f7db0239765ab36788f425dc00d9570384d6
Diego Cezar <diegocezar.comercial@gmail.com>
2019-07-08 22:47:06 -0300

    Desafio avançado Aula46 - botao voltar funcionando

--- src/app.js
+++ src/app.js
@@ -10,4 +10,9 @@
   //Testa se o hash é true ou false, se for undefined retorna false e vai
   //para o erro
   path[hash] ? path[hash]() : path["#/404"]();
+
+  //Voltar para página anterior com botão do browser e do celular
+  window.onpopstate = () => {
+    location.reload(true);
+  };
 })();
